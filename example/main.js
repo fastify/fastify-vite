7032eb93ba3a9d84a57222204c4a099630b4d353
Jonas Galvez <jonasgalvez@gmail.com>
2021-03-13 22:54:12 -0300

    fix: remove dead code

--- example/main.js
+++ example/main.js
@@ -15,20 +15,3 @@ export function createApp (req) {
   app.use(head)
   return { ctx, app, head, router }
 }
-
-export async function useSSRData (fetchOptions = {}) {
-  const appConfig = getCurrentInstance().appContext.app.config
-  const ssrDataPath = appConfig.globalProperties.$ssrDataPath()
-  let ssrData = ref(appConfig.globalProperties.$ssrData)
-  const ssrDataRefresh = async (overrideOptions = {}) => {
-    const response = await fetch(ssrDataPath, assign(fetchOptions, overrideOptions))
-    const { headers, status } = response
-    const body = await response.json()
-    return { headers, status, body }
-  }
-  if (!ssrData.value && !import.meta.env.SSR) {
-    const { body } = await ssrDataRefresh()
-    ssrData = ref(body)
-  }
-  return [ssrData, ssrDataRefresh, ssrDataPath]
-}
