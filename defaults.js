d8049db093a0418fa298c32f498ade36edc90464
Jonas Galvez <jonasgalvez@gmail.com>
2021-03-25 15:55:36 -0300

    update deps, handler

--- defaults.js
+++ defaults.js
@@ -3,6 +3,7 @@ module.exports = {
   dataKey: '$data',
   globalDataKey: '$global',
   rootDir: process.cwd(),
+  assetsDir: 'assets',
   clientEntryPath: '/entry-client.js',
   serverEntryPath: '/entry-server.js'
 }
