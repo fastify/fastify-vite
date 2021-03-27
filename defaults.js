7bbeac9b30483b6d63a2551136bf8c9d82d825f4
Jonas Galvez <jonasgalvez@gmail.com>
2021-03-26 22:54:43 -0300

    rm defaults

--- defaults.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dev: process.env.NODE_ENV !== 'production',
-  dataKey: '$data',
-  globalDataKey: '$global',
-  rootDir: process.cwd(),
-  assetsDir: 'assets',
-  clientEntryPath: '/entry-client.js',
-  serverEntryPath: '/entry-server.js'
-}
