5061a9dbd529974f4651de8b68b0dc44a2a99190
Jonas Galvez <jonasgalvez@gmail.com>
2021-03-26 22:02:21 -0300

    add new options.js

---
+++ options.js
@@ -0,0 +1,70 @@
+const { assign } = Object
+const { resolve } = require('path')
+// const vuePlugin = require('@vitejs/plugin-vue')
+// const vueJsx = require('@vitejs/plugin-vue-jsx')
+
+const defaults = {
+  dev: process.env.NODE_ENV !== 'production',
+  hydration: {
+    global: '$global',
+    data: '$data',
+    api: '$api',
+  },
+  root: process.cwd(),
+  entry: {
+    client: '/entry/client.js',
+    server: '/entry/server.js',
+  },
+  vite: {
+    // plugins: [
+    //   vuePlugin(),
+    //   vueJsx()
+    // ],
+    build: {
+      minify: false
+    },
+  },
+}
+
+function getOptions (options) {
+  options = assign({}, defaults, options)
+  if (typeof options.root === 'function') {
+    options.root = options.root(resolve)
+  }
+  return options
+}
+
+async function patchOptions (options) {
+  const viteOptions = await getViteOptions(options)
+
+  options.vite.build.assetsDir = viteOptions.build.assetsDir
+
+  if (!options.dev) {
+    options.distDir = resolve(options.root, viteOptions.build.outDir)
+    const distIndex = resolve(options.distDir, 'client/index.html')
+    if (!existsSync(distIndex)) {
+      throw new Error('Missing production client/index.html — did you build first?')
+    }
+    options.distIndex = readFileSync(distIndex, 'utf8')
+    options.distManifest = require(resolve(options.distDir, 'client/ssr-manifest.json'))
+  } else {
+    options.distManifest = []
+  }
+  return options
+}
+
+module.exports = { defaults, getOptions, patchOptions }
+
+function getViteOptions (options) {
+  const mergedOptions = { root: options.root, ...defaults.vite, ...options.vite }
+  // If vite.config.js is present, resolveConfig() ensures it's taken into consideration
+  // Note however that vite options set via fastify-vite take precedence over vite.config.js
+  return {
+    root: options.root,
+    build: {
+      assetsDir: 'assets',
+      outDir: 'dist',
+    }
+  }
+}
+
