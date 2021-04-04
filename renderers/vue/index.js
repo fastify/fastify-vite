541d202d74222dc1fc5e52ae4e92c261721fe602
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-04 11:02:39 -0300

    wip

--- renderers/vue/index.js
+++ renderers/vue/index.js
@@ -1,2 +1,11 @@
 const { options } = require('./options')
 const { getHandler, getRenderGetter } = require('./handler')
+const { renderDevHTMLTemplate, renderHTMLTemplate } = require('./html')
+
+module.exports = { 
+  options, 
+  getHandler,
+  getRenderGetter,
+  renderDevHTMLTemplate,
+  renderHTMLTemplate,
+}
