430409a7a5b34e22f72290e208ce27d3142e8618
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-11 18:49:07 -0300

    fix(react): missing dev/production template distinction

--- renderers/react/html.js
+++ renderers/react/html.js
@@ -1,17 +1,29 @@
-exports.renderHTMLTemplate = (req, { element, hydration, entry, helmet }) => {
-  return (`
-    <!doctype html>
-      <head>
-        ${helmet.title.toString()}
-        ${helmet.script.toString()}
-        ${helmet.style.toString()}
-      </head>
-      <body>
-      <body>
-        ${hydration}
-        <div id="root">${element}</div>
-        <script type="module" src="${entry}"></script> 
-      </body>
-    </html>
-  `)
+function renderDevHTMLTemplate (req, { element, hydration, entry, helmet }) {
+  return (
+    '<!doctype html>\n' + 
+    `<head>${
+      helmet.title.toString()
+    }${
+      helmet.script.toString()
+    }${
+      helmet.style.toString()
+    }</head>\n` +
+    `<body>\n${hydration}\n` +
+    `<div id="root">${element}</div>\n` +
+    `<script type="module" src="${entry}"></script>\n` +
+    '</body>\n' +
+    '</html>\n'
+  )
 }
+
+function renderHTMLTemplate (req, { attrs, head, element, hydration }, template) {
+  return template
+    .replace('<html>', `<html${attrs.html}>`)
+    .replace('<body>', `<body${attrs.body}>`)
+    .replace('<!--head.preload-->', head.preload)
+    .replace('<!--head.tags-->', head.tags || '')
+    .replace('<!--hydration-->', hydration)
+    .replace('<!--element-->', element)
+}
+
+module.exports = { renderHTMLTemplate, renderDevHTMLTemplate }
