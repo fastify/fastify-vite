6a0ff7869f1fd3b3589f113fb1eda8056e199017
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-11 17:56:46 -0300

    missing react/options

---
+++ renderers/react/options.js
@@ -0,0 +1,28 @@
+const reactRefresh = require('@vitejs/plugin-react-refresh')
+
+const dev = process.env.NODE_ENV !== 'production'
+
+const options = {
+  // Vite's logging level
+  logLevel: dev ? 'error' : 'info',
+  // Vite plugins needed for Vue 3 SSR to fully work
+  plugins: [
+    vuePlugin(),
+    vueJsx()
+  ],
+  esbuild: {
+    jsxInject: `import React from 'react';`
+  },
+  plugins: [
+    reactRefresh()
+  ],
+  // Base build settings, default values
+  // for assetsDir and outDir match Vite's defaults
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+    minify: !dev
+  }
+}
+
+module.exports = { options }
