5b1424aecbac956e4121fc72451304934177a59c
Jonas Galvez <jonasgalvez@gmail.com>
2021-06-13 19:57:09 -0300

    add fastify-vite-vue/plugin

---
+++ packages/fastify-vite-vue/plugin.js
@@ -0,0 +1,17 @@
+const { readFileSync } = require('fs')
+const { resolve } = require('path')
+
+const clientSource = readFileSync(resolve(__dirname, 'client.js'), 'utf8')
+
+module.exports = () => ({
+  resolveId (id) {
+    if (id === '@fastify-vite-vue/client') {
+      return '@fastify-vite-vue/client'
+    }
+  },
+  load (id) {
+    if (id === '@fastify-vite-vue/client') {
+      return clientSource
+    }
+  }
+})
