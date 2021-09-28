501ce84abb1558ce0f2814706107b46db94d6827
Jonas Galvez <jonasgalvez@gmail.com>
2021-09-28 09:06:12 -0300

    fix vue-base

--- examples/vue-base/views/about.vue
+++ examples/vue-base/views/about.vue
@@ -1,22 +1,7 @@
 <template>
-  <h2>Accessing global data from the server</h2>
-
-  <!-- Global Data comes from req.$global and it's already injected as globalProperties -->
-  <p>{{ $global }}</p>
-
-  <!-- You can also access it from setup() via useHydration() and make it available in other ways -->
-  <p>{{ foobar }}</p>
+  <h1>About Page</h1>
 </template>
 
 <script>
-import { useHydration } from 'fastify-vite-vue/client'
-
-export const path = '/global-data'
-
-export default {
-  async setup () {
-    const ctx = await useHydration()
-    return { foobar: ctx.$global }
-  }
-}
+export const path = '/about'
 </script>
--- examples/vue-base/views/index.vue
+++ examples/vue-base/views/index.vue
@@ -1,5 +1,6 @@
 <template>
   <h1>Index Page</h1>
+  <p><router-link to="/about">Go to /about</router-link></p>
 </template>
 
 <script>
