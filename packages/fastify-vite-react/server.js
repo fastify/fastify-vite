57f7e55b68d82b0d84124bbadfc7330c2e7b6f3b
Jonas Galvez <jonasgalvez@gmail.com>
2021-10-01 09:28:10 -0300

    add useFastify, useRequest and useReply to react adapter

--- packages/fastify-vite-react/server.js
+++ packages/fastify-vite-react/server.js
@@ -1,3 +1,29 @@
 const { createRenderFunction } = require('./render')
+const { Context } = require('./context')
 
-module.exports = { createRenderFunction }
+const isServer = typeof window === 'undefined'
+
+function useRequest () {
+  if (isServer) {
+    return useContext(Context).req
+  }
+}
+
+function useReply () {
+  if (isServer) {
+    return useContext(Context).reply
+  }
+}
+
+function useFastify () {
+  if (isServer) {
+    return useContext(Context).fastify
+  }
+}
+
+module.exports = {
+  createRenderFunction,
+  useRequest,
+  useReply,
+  useFastify,
+}
