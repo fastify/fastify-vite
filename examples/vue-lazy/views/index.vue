ac68def0a1199f246d9f297343b0e40644219790
Jonas Galvez <jonasgalvez@gmail.com>
2021-10-05 11:43:29 -0300

    add vue-lazy example

---
+++ examples/vue-lazy/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Index Page</h1>
+  <p><router-link to="/about">Go to /about</router-link></p>
+</template>
+
+<script>
+import { useFastify } from 'fastify-vite-vue/server'
+import { packIsland } from 'fastify-vite-vue/client'
+
+export const path = '/'
+
+export const onSend = packIsland()
+
+export default {
+  async setup () {
+    const fastify = useFastify()
+    // console.log(typeof fastify)
+    return
+  }
+}
+</script>
