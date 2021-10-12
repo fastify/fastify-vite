819768ef404daeea59aecdf0ce6ebb24d8bf40bd
Jonas Galvez <jonasgalvez@gmail.com>
2021-10-12 20:52:30 -0300

    fix

--- examples/vue-island/entry/client.js
+++ examples/vue-island/entry/client.js
@@ -2,7 +2,7 @@
 import { onIdle } from 'fastify-vite/client'
 import { hydrate } from 'fastify-vite-vue/client'
 
-document.querySelector('#app').addEventListener('mouseover', async () => {
+onIdle(async () => {
   const { createApp } = await import('../client')
   const { app } = createApp()
   hydrate(app)
