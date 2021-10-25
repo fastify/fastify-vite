1316d04f421ed24b4c63e5a1745940f29d2d795f
Jonas Galvez <jonasgalvez@gmail.com>
2021-10-25 16:13:30 -0300

    bump

--- packages/fastify-vite-vue/base/client.vue
+++ packages/fastify-vite-vue/base/client.vue
@@ -1,7 +1,7 @@
 <template>
   <router-view v-slot="{ Component }">
     <Suspense>
-      <component :key="route.path" :is="Component" />
+      <component :key="$route.path" :is="Component" />
     </Suspense>
   </router-view>
 </template>
