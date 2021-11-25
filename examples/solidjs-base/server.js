f7a390fd37ed82b7bc25c2daaeabd3c8b30b1b0c
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2021-11-25 18:36:24 +0200

    setting up base example

---
+++ examples/solidjs-base/server.js
@@ -0,0 +1,27 @@
+const Fastify = require('fastify')
+const FastifyVite = require('fastify-vite')
+const FastifyViteSolid = require('fastify-vite-solidjs')
+
+async function main () {
+  const app = Fastify()
+  await app.register(FastifyVite, {
+    root: __dirname,
+    renderer: FastifyViteSolid,
+  })
+  await app.vite.commands()
+  return app
+}
+
+if (require.main === module) {
+  main().then((app) => {
+    app.listen(3000, (err, address) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      console.log(`Server listening on ${address}`)
+    })
+  })
+}
+
+module.exports = main
