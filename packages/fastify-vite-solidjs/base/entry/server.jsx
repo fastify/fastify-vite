ed1df5c773e7e5fb37e6ece240843a92f9e4eb6d
yonatan.bendahan@gmail.com <yonatan.bendahan@gmail.com>
2021-11-25 18:31:51 +0200

    fix entry imports

--- packages/fastify-vite-solidjs/base/entry/server.jsx
+++ packages/fastify-vite-solidjs/base/entry/server.jsx
@@ -1,4 +1,4 @@
-import { createRenderFunction } from 'fastify-vite-react/server'
+import { createRenderFunction } from 'fastify-vite-solidjs/server'
 import { createApp } from '@app/client.jsx'
 import routes from '@app/routes.js'
 
