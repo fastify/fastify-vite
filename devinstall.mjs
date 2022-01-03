41d056b0923d3e38fd705ba81afcb40fbd8ea369
Jonas Galvez <jonasgalvez@gmail.com>
2022-01-03 01:24:05 -0300

    fastify-vite-vue-classic wip

--- devinstall.mjs
+++ devinstall.mjs
@@ -1,73 +1,48 @@
 
-import { fileURLToPath } from 'url'
+const root = path.resolve(__dirname)
+const { name: example } = path.parse(process.cwd())
+const exRoot = path.resolve(__dirname, 'examples', example)
 
-$.quote = s => s
-
-const { entries } = Object
-
-let root = fileURLToPath(path.dirname(import.meta.url))
-let [example, renderer, exRoot, deps] = await parseArgv(root)
-
-let pkgInfos = {}
-
-await cd(root)
+if (!fs.existsSync(exRoot)) {
+  console.log('Must be called from a directory under examples/.')
+  process.exit()
+}
 
 await $`rm -rf ${exRoot}/node_modules/vite`
 await $`rm -rf ${exRoot}/node_modules/.vite`
 
-await $`mv ${root}/${exRoot}/package.json ${root}/${exRoot}/package.dist.json`
-await $`mv ${root}/${exRoot}/package.dev.json ${root}/${exRoot}/package.json`
+const deps = require(path.join(exRoot, 'package.deps.json'))
+const localPackages = fs.readdirSync(path.join(root, 'packages'))
 
-for (let pkg of ['fastify-vite', renderer]) {
-  await $`rm -rf ${exRoot}/node_modules/${pkg}`
-  let pkgRoot = `${root}/packages/${pkg}`
-  let pkgInfo = await readJSON(`${pkgRoot}/package.json`)
-  pkgInfos[pkg] = pkgInfo
-  const subDeps = entries(pkgInfo.dependencies).map(([n, v]) => `${n}@${v}`)
-  await cd(`${root}/${exRoot}`)
-  await $`npm install --silent --force ${deps.external.join(' ')} ${subDeps.join(' ')}`
-  await cd(root)
-}
+const externalDeps = Object.fromEntries(Object.entries(deps).filter(([dep]) => {
+  return !localPackages.includes(dep)
+}))
 
-await $`mv ${root}/${exRoot}/package.json ${root}/${exRoot}/package.dev.json`
-await $`mv ${root}/${exRoot}/package.dist.json ${root}/${exRoot}/package.json`
+const devDeps = Object.fromEntries(Object.entries(deps).filter(([dep]) => {
+  return localPackages.includes(dep)
+}))
 
-// Hard copy packages after all calls to npm install have ended
-// If you run npm install on the example folder, you also need to run devinstall again
-for (let pkg of ['fastify-vite', renderer]) {
-  await $`cp -r ${root}/packages/${pkg} ${root}/${exRoot}/node_modules/${pkg}`
-}
-
-async function getDeps (example, exRoot) {
-  const examplePackage = await readJSON(`${root}/${exRoot}/package.json`)
-  const pkgInfo = await fs.readdir(path.join(root, 'packages'))
-  return {
-    local: Object.keys(examplePackage.dependencies).filter(dep => pkgInfo.includes(dep)),
-    external: Object.keys(examplePackage.dependencies).filter(dep => !pkgInfo.includes(dep))
+for (const devDep of Object.keys(devDeps)) {
+  for (const [dep, version] of Object.entries(
+    require(path.join(root, 'packages', devDep, 'package.json')).dependencies)
+  ) {
+    externalDeps[dep] = version
   }
 }
 
-async function parseArgv () {
-  const example = process.argv[3]
-  const renderer = process.argv[4]
-  const exRoot = `examples/${example}`
-  const deps = await getDeps(path.join(exRoot, 'package.json'), exRoot)
-  if (!example || !renderer) {
-    console.error('Usage: npm run devinstall -- <dir>')
-    process.exit(1)
-  }
-  if (!await fs.stat(path.join(root, exRoot)).catch(() => false)) {
-    console.error(`Directory ${join(root, exRoot)} does not exist.`)
-    process.exit(1)    
-  }
-  return [example, renderer, exRoot, deps]
+await updateDeps(exRoot, externalDeps)
+await $`npm install -f`
+
+for (const devDep of Object.keys(devDeps)) {
+  await $`cp -r ${root}/packages/${devDep} ${exRoot}/node_modules/${devDep}`
 }
 
-async function readJSON(path) {
-  const json = await fs.readFile(path, 'utf8')
-  return JSON.parse(json)
-}
-
-async function writeJSON(path, contents) {
-  await fs.writeFile(path, contents)
+async function updateDeps(exRoot, dependencies) {
+  await fs.writeFile(
+    path.join(exRoot, 'package.json'),
+    JSON.stringify({
+      ...require(path.join(exRoot, 'package.json')),
+      dependencies,
+    }, null, 2)
+  )
 }
---
+++ packages/fastify-vite-vue-classic/base/client.vue
@@ -0,0 +1,43 @@
+<template>
+  <template v-if="error">
+    <Error :error="error" />
+  </template>
+  <template v-else>
+    <Router />
+  </template>
+</template>
+
+<script>
+// import { ref, computed, onErrorCaptured } from 'vue'
+// import { useHead } from '@vueuse/head'
+// import { useIsomorphic } from 'fastify-vite-vue-classic/app'
+
+import Router from '@app/router.vue'
+import Error from '@app/error.vue'
+
+import * as head from '@app/head.js'
+
+export default {
+  components: {
+    Error,
+    Router,
+  },
+  created () {
+    // // const ctx = useIsomorphic()
+
+    // // if (typeof head?.default === 'function') {
+    // //   useHead(head.default(ctx))
+    // // } else {
+    // //   useHead(head.default ?? head)
+    // // }
+
+    // onErrorCaptured((error) => {
+    //   ctx.$error = error
+    // })
+
+    // return {
+    //   error: computed(() => ctx.$error)
+    // }
+  },
+}
+</script>
