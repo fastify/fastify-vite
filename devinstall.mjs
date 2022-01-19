481131415abbb7138bf560296f997aa45c33c9d6
Jonas Galvez <jonasgalvez@gmail.com>
2022-01-19 12:43:09 -0300

    devinstall fixes

--- devinstall.mjs
+++ devinstall.mjs
@@ -3,6 +3,7 @@ import chokidar from 'chokidar'
 const root = path.resolve(__dirname)
 const { name: example } = path.parse(process.cwd())
 const exRoot = path.resolve(__dirname, 'examples', example)
+const command = process.argv.slice(5)
 
 if (!fs.existsSync(exRoot)) {
   console.log('Must be called from a directory under examples/.')
@@ -33,18 +34,21 @@ const watchers = []
 
 for (const localDep of Object.keys(local)) {
   await $`cp -r ${root}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
-  setImmediate(() => {
-    const watcher = chokidar.watch(`${root}/packages/${localDep}`, {
-      ignored: [/node_modules/],
-      ignoreInitial: true,
-    })
-    const changed = () => $`cp -r ${root}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
-    watcher.on('add', changed)
-    watcher.on('unlink', changed)
-    watcher.on('change', changed)
-    watchers.push(changed)
+  const changed = (reason) => async (path) => {
+    console.log(`ℹ ${reason} ${path}`)
+    await $`cp -r ${root}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
+  }
+  const watcher = chokidar.watch(`${root}/packages/${localDep}`, {
+    ignored: [/node_modules/],
+    ignoreInitial: true,
   })
+  watcher.on('add', changed('A'))
+  watcher.on('unlink', changed('D'))
+  watcher.on('change', changed('M'))
+  watchers.push(changed)
 }
+
+await $`${command}`
 
 async function createPackageFile(exRoot, dependencies) {
   await fs.writeFile(
