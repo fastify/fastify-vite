92d1af1481f85cf978a730efad3d0a28653980be
Jonas Galvez <jonasgalvez@gmail.com>
2022-01-20 04:36:31 -0300

    fix

--- devinstall.mjs
+++ devinstall.mjs
@@ -1,6 +1,5 @@
 
 import chokidar from 'chokidar'
-const root = path.resolve(__dirname)
 const { name: example } = path.parse(process.cwd())
 const exRoot = path.resolve(__dirname, 'examples', example)
 const command = process.argv.slice(5)
@@ -14,14 +13,14 @@ await $`rm -rf ${exRoot}/node_modules/vite`
 await $`rm -rf ${exRoot}/node_modules/.vite`
 
 const template = require(path.join(exRoot, 'package.json'))
-const localPackages = fs.readdirSync(path.join(root, 'packages'))
+const localPackages = fs.readdirSync(path.join(__dirname, 'packages'))
 
 const { external, local } = template
 const dependencies = { ...external }
 
 for (const localDep of Object.keys(local)) {
   for (const [dep, version] of Object.entries(
-    require(path.join(root, 'packages', localDep, 'package.json')).dependencies)
+    require(path.join(__dirname, 'packages', localDep, 'package.json')).dependencies)
   ) {
     dependencies[dep] = version
   }
@@ -31,12 +30,12 @@ await createPackageFile(exRoot, dependencies)
 await $`npm install -f`
 
 for (const localDep of Object.keys(local)) {
-  await $`cp -r ${root}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
+  await $`cp -r ${__dirname}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
   const changed = (reason) => async (path) => {
     console.log(`ℹ ${reason} ${path}`)
-    await $`cp -r ${root}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
+    await $`cp -r ${__dirname}/packages/${localDep} ${exRoot}/node_modules/${localDep}`
   }
-  const watcher = chokidar.watch(`${root}/packages/${localDep}`, {
+  const watcher = chokidar.watch(`${__dirname}/packages/${localDep}`, {
     ignored: [/node_modules/],
     ignoreInitial: true,
   })
