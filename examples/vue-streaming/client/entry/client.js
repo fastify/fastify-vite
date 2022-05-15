d57e2728a2408cd4d74faf447d3feff33d4ea192
Jonas Galvez <jonasgalvez@gmail.com>
2022-05-14 22:32:31 -0300

    rm example

--- examples/vue-streaming/client/entry/client.js
+++
@@ -1,8 +0,0 @@
-import './client.css'
-import { createApp } from './app.js'
-
-createApp().then(({ instance, router }) => {
-  router.isReady().then(() => {
-    instance.mount('main')
-  })
-})
--- examples/vue-streaming/client/other.vue
+++
@@ -1,4 +0,0 @@
-<template>
-	<p>This page is just for demonstrating client-side navigation.</p>
-	<router-link to="/">Go back to index</router-link>
-</template>
