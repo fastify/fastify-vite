5fa27cd7ef0797330e7dbc8ce7d764d4f56eb3d4
Anthony Mittaz <sync@me.com>
2022-05-19 14:47:28 +1000

    fix dev, prod config setup
    not hardcoding building in production all the time

--- packages/fastify-vite/config.js
+++ packages/fastify-vite/config.js
@@ -88,11 +88,12 @@ const DefaultConfig = {
 }
 
 async function configure (options = {}) {
-  const defaultConfig = Object.create(DefaultConfig)
+  const defaultConfig = { ...DefaultConfig }
   const root = resolveRoot(options.root)
-  const [vite, viteConfig] = await resolveViteConfig(root)
+  const dev = typeof options.dev === 'boolean' ? options.dev : defaultConfig.dev
+  const [vite, viteConfig] = await resolveViteConfig(root, dev)
   const clientModule = defaultConfig.clientModule ?? resolveClientModule(vite.root)
-  const bundle = await resolveBundle({ ...options, vite })
+  const bundle = await resolveBundle({ dev, vite })
   const config = Object.assign(defaultConfig, {
     ...options,
     vite,
@@ -136,12 +137,16 @@ function resolveRoot (root) {
   }
 }
 
-async function resolveViteConfig (root) {
+async function resolveViteConfig (root, dev) {
   for (const ext of ['js', 'mjs', 'ts', 'cjs']) {
     const configFile = join(root, `vite.config.${ext}`)
     if (exists(configFile)) {
       return [
-        await resolveConfig({ configFile }, 'build', 'production'),
+        await resolveConfig(
+          { configFile },
+          "build",
+          dev ? "development" : "production"
+        ),
         configFile,
       ]
     }
