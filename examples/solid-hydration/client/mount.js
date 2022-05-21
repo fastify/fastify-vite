b379fa2c1c42a354211ec9ecd0796f936bc0f4ce
Jonas Galvez <jonasgalvez@gmail.com>
2022-05-21 10:51:58 -0300

    examples: solid-hydration

--- examples/solid-hydration/client/mount.js
+++ examples/solid-hydration/client/mount.js
@@ -2,6 +2,8 @@ import './base.css'
 import { hydrate } from 'solid-js/web'
 import { createApp } from './base.jsx'
 
+// Patch to address hydrate() error
+// Reported to SolidJS's author for investigation
 globalThis._$HY = {}
 
 hydrate(
