1e5ef0f5b4bc2422b219901955d43c5ffc265e4f
bskimball <brian.kimball@bdkonline.com>
2022-07-05 21:24:02 -0400

    changed scripts to relative urls for windows. on production builds, check if windows, if is windows do a dynamic import using the file path.

--- examples/solid-hydration/vite.config.js
+++ examples/solid-hydration/vite.config.js
@@ -1,10 +1,9 @@
-import { join, dirname } from 'path'
 import viteSolid from 'vite-plugin-solid'
 import viteFastify from 'fastify-vite/plugin'
 
 // @type {import('vite').UserConfig}
 export default {
-  root: join(dirname(new URL(import.meta.url).pathname), 'client'),
+  root: './client',
   plugins: [
     viteSolid({ ssr: true }),
     viteFastify(),
--- examples/svelte-vanilla/vite.config.js
+++ examples/svelte-vanilla/vite.config.js
@@ -1,10 +1,9 @@
-import { join, dirname } from 'path'
 import { svelte as viteSvelte } from '@sveltejs/vite-plugin-svelte'
 import viteFastify from 'fastify-vite/plugin'
 
 // @type {import('vite').UserConfig}
 export default {
-  root: join(dirname(new URL(import.meta.url).pathname), 'client'),
+  root: './client',
   plugins: [
     viteSvelte(),
     viteFastify(),
