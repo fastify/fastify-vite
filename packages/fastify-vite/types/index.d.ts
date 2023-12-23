405ad871eceda3d257b3c5d2a16a37fcabf0fb6b
gehbert <rgehbt@gmail.com>
2023-12-23 12:24:52 +0800

    fix: truncated columns

--- packages/fastify-vite/types/index.d.ts
+++ packages/fastify-vite/types/index.d.ts
@@ -35,22 +35,53 @@ type RouteType = Partial<{
   serverOnly: unknown
 }>
 type Loosen<T> = T & Record<string, unknown>
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-type Ctx = Loosen<{ routes: Array<RouteType>, context: unknown, body: unknown, stream: unknown, data: unknown }>
-
+type Ctx = Loosen<{
+  routes: Array<RouteType>,
+  context: unknown,
+  body: unknown,
+  stream: unknown,
+  data: unknown
+}>
 interface RendererFunctions {
   createHtmlTemplateFunction(source: string): unknown
-  createHtmlFunction(source: string, scope?: unknown, config?: unknown): (ctx: Ctx) => Promise<unknown>;
-  createRenderFunction(args: Loosen<{ routes: Array<RouteType>, create: (arg0: Record<string, unknown>) => unknown, createApp: unknown }>): Promise<(server: unknown, req: unknown, reply: unknown) => Ctx | { element:string, hydration:string }>
+  createHtmlFunction(
+    source: string,
+    scope?: unknown,
+    config?: unknown
+  ): (ctx: Ctx) => Promise<unknown>;
+  createRenderFunction(args: Loosen<{
+    routes: Array<RouteType>,
+    create: (arg0: Record<string, unknown>) => unknown,
+    createApp: unknown
+  }>): Promise<(
+    server: unknown,
+    req: unknown,
+    reply: unknown
+  ) => Ctx | { element: string, hydration: string }>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-interface RendererOption<ClientModuleType = string | Record<string, unknown> | unknown, ClientType = unknown> extends RendererFunctions {
-  clientModule: ClientModuleType
-  createErrorHandler(client: ClientType, scope: FastifyInstance, config?: unknown): (error: Error, req?: FastifyRequest, reply?: FastifyReply) => void
-  createRoute(args: Loosen<{ client?: ClientType, handler?: (...args: unknown[]) => unknown, errorHandler: (error: Error, req?: FastifyRequest, reply?: FastifyReply) => void, route?: RouteType }>, scope: FastifyInstance, config: unknown): void
-  createRouteHandler(client: ClientType, scope: FastifyInstance, config?: unknown): (req: FastifyRequest, reply: FastifyReply) => Promise<unknown>
-  prepareClient(clientModule: ClientModuleType, scope?: FastifyInstance, config?: unknown): Promise<ClientType>
+interface RendererOption<CM = string | Record<string, unknown> | unknown, C = unknown> extends RendererFunctions {
+  clientModule: CM
+  createErrorHandler(client: C, scope: FastifyInstance, config?: unknown):
+    (error: Error, req?: FastifyRequest, reply?: FastifyReply) => void
+  createRoute(args: Loosen<{
+    client?: C,
+    handler?: (...args: unknown[]) => unknown,
+    errorHandler: (
+      error: Error,
+      req?: FastifyRequest,
+      reply?: FastifyReply
+    ) => void,
+    route?: RouteType
+  }>, scope: FastifyInstance, config: unknown): void
+  createRouteHandler(client: C, scope: FastifyInstance, config?: unknown):
+    (req: FastifyRequest, reply: FastifyReply) => Promise<unknown>
+  prepareClient(
+    clientModule: CM,
+    scope?: FastifyInstance,
+    config?: unknown
+  ): Promise<C>
 }
 
 declare namespace fastifyVite {
