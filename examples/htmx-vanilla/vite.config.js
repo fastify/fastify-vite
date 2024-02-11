b4c77902014843e91647c8ed879ee304e0a53a6c
Jonas Galvez <jonasgalvez@gmail.com>
2024-02-11 02:26:07 -0300

    fix: build options (2)

--- examples/htmx-vanilla/vite.config.js
+++ examples/htmx-vanilla/vite.config.js
@@ -4,36 +4,39 @@ import inject from '@rollup/plugin-inject'
 
 const __dirname = dirname(new URL(import.meta.url).pathname)
 
-export default {
-  root: join(__dirname, 'client'),
-  esbuild: {
-    jsxFactory: 'Html.createElement',
-    jsxFragment: 'Html.Fragment',
-  },
-  optimizeDeps: {
-    include: ['/client/**']
-  },
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: [
-        resolve(__dirname, 'client/client.js'),
-        resolve(__dirname, 'client/server.jsx'),
-      ],
+export default function ({ isSsrBuild }) {
+  return {
+    root: join(__dirname, 'client'),
+    esbuild: {
+      jsxFactory: 'Html.createElement',
+      jsxFragment: 'Html.Fragment',
     },
-    rollupOptions: {
-      external: ['htmx.org'],
-      output: {
-        exports: 'named',
-      }
+    optimizeDeps: {
+      include: ['/client/**']
     },
-  },
-  plugins: [
-    inject({
-      // See https://github.com/bigskysoftware/htmx/issues/1469
-       htmx: 'htmx.org',
-       // Ensures @kitajs/html availability for JSX
-       Html: '@kitajs/html'
-    }),
-  ],
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: isSsrBuild 
+          ? resolve(__dirname, 'client/server.jsx')
+          : resolve(__dirname, 'client/client.js'),
+        fileName: 'index',
+        formats: ['es'],
+      },
+      rollupOptions: {
+        external: ['htmx.org'],
+        output: {
+          exports: 'named',
+        }
+      },
+    },
+    plugins: [
+      inject({
+        // See https://github.com/bigskysoftware/htmx/issues/1469
+         htmx: 'htmx.org',
+         // Ensures @kitajs/html availability for JSX
+         Html: '@kitajs/html'
+      }),
+    ],
+  }
 }
