db874f28cca3bda952030c4323b77eaa2c4c4efd
Jonas Galvez <jonasgalvez@gmail.com>
2024-02-19 05:01:23 -0300

    fix

--- examples/htmx-vanilla/client/client.js
+++
@@ -1,8 +0,0 @@
-const pages = import.meta.glob('/pages/*.jsx')
-
-pages[document.documentElement.dataset.module]()
-
-!async function () {
-  const { default: htmx } = await import('htmx.org')
-  window.htmx = htmx
-}()
---
+++ starters/htmx-base/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './client/index.html',
+    './client/**/*.{mjs,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
