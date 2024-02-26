beb415c9946ca7042372c8ef1aa575af220b9e0a
Jonas Galvez <jonasgalvez@gmail.com>
2024-02-26 00:01:18 -0300

    chore(vue): update versions in starter templates

--- starters/vue-kitchensink/client/root.vue
+++ starters/vue-kitchensink/client/root.vue
@@ -1,6 +1,8 @@
 <script>
 export { default } from '/:router.vue'
 
+export const mount = '#root'
+
 export function configure ({ app, router }) {
   // Use this to configure/extend your Vue app and router instance
 }
