ee815affd2343e6ef4567d4f49f78eb272c15166
Jonas Galvez <jonasgalvez@gmail.com>
2024-09-26 04:29:31 -0300

    chore: update vue starters vite configs

--- starters/vue-kitchensink/vite.config.js
+++ starters/vue-kitchensink/vite.config.js
@@ -2,6 +2,7 @@ import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 import viteVue from '@vitejs/plugin-vue'
+import viteFastify from '@fastify/vite/plugin'
 import viteFastifyVue from '@fastify/vue/plugin'
 
 const path = fileURLToPath(import.meta.url)
@@ -10,6 +11,7 @@ export default {
   root: join(dirname(path), 'client'),
   plugins: [
     viteVue(), 
+    viteFastify(),
     viteFastifyVue(),
   ],
 }
