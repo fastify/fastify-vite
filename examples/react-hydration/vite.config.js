2de5709165cb72062be2f96725dd9c6ea3fc1119
Wei Wang <wei@fastmail.com>
2024-10-12 13:10:26 -0400

    Fix import path of all examples

--- examples/react-hydration/vite.config.js
+++ examples/react-hydration/vite.config.js
@@ -1,6 +1,6 @@
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { viteFastify } from '@fastify/vite'
+import { viteFastify } from '@fastify/vite/plugin'
 import viteReact from '@vitejs/plugin-react'
 
 const path = fileURLToPath(import.meta.url)
