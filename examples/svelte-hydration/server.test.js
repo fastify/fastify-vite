f98b75ad0e463361a33f5d453d3db6b9ef2cb51a
Jonas Galvez <jonasgalvez@gmail.com>
2024-10-13 03:45:29 -0300

    fix: svelte-hydration test

--- examples/svelte-hydration/server.test.js
+++ examples/svelte-hydration/server.test.js
@@ -1,4 +1,5 @@
 import test from 'node:test'
+import { setTimeout } from 'node:timers/promises'
 import { join, resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { makeSSRBuildTest, makeIndexTest } from '../test-factories.mjs'
@@ -6,6 +7,9 @@ import { main } from './server.js'
 
 const cwd = dirname(fileURLToPath(import.meta.url))
 
-test('build production bundle', makeSSRBuildTest({ cwd, clientModules: 25, serverModules: 23 }))
-test('render index page in production', makeIndexTest({ main }))
-test('render index page in development', makeIndexTest({ main, dev: true }))
+test('svelte-hydration', async (t) => {
+  await t.test('build production bundle', makeSSRBuildTest({ cwd, clientModules: 25, serverModules: 23 }))
+  await t.test('render index page in production', makeIndexTest({ main }))
+  await setTimeout(1000)
+  await t.test('render index page in development', makeIndexTest({ main, dev: true }))
+})
