0cd3f4b35c64dd0231a62e189b8f63f8cb0683ca
Jonas Galvez <jonasgalvez@gmail.com>
2024-10-13 17:31:03 -0300

    fix(vue-kitchensink): correct vite root

--- starters/vue-kitchensink/vite.config.js
+++ starters/vue-kitchensink/vite.config.js
@@ -1,9 +1,10 @@
+import { join } from 'node:path'
 import viteVue from '@vitejs/plugin-vue'
 import viteFastify from '@fastify/vite/plugin'
 import viteFastifyVue from '@fastify/vue/plugin'
 
 export default {
-  root: import.meta.dirname,
+  root: join(import.meta.dirname, 'client'),
   plugins: [
     viteVue(), 
     viteFastify(),
