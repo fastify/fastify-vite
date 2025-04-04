d7278e79fcd728c059108aca901a474df0db911c
Jonas Galvez <jonasgalvez@gmail.com>
2025-04-03 21:49:36 -0300

    update starters

--- starters/vue-base/vite.config.js
+++ starters/vue-base/vite.config.js
@@ -1,11 +1,11 @@
 import { join } from 'node:path'
-import viteVue from '@vitejs/plugin-vue'
 import viteFastifyVue from '@fastify/vue/plugin'
+import viteVue from '@vitejs/plugin-vue'
 
 export default {
   root: join(import.meta.dirname, 'client'),
   plugins: [
-    viteVue(),
     viteFastifyVue(),
+    viteVue(),
   ],
 }
--- starters/vue-kitchensink/vite.config.js
+++ starters/vue-kitchensink/vite.config.js
@@ -1,13 +1,11 @@
 import { join } from 'node:path'
-import viteVue from '@vitejs/plugin-vue'
-import viteFastify from '@fastify/vite/plugin'
 import viteFastifyVue from '@fastify/vue/plugin'
+import viteVue from '@vitejs/plugin-vue'
 
 export default {
   root: join(import.meta.dirname, 'client'),
   plugins: [
-    viteVue(),
-    viteFastify(),
     viteFastifyVue(),
+    viteVue(),
   ],
 }
