e5bb641b085a71ed5b8db982adb111b030b2c7d0
Jonas Galvez <jonasgalvez@gmail.com>
2025-04-20 19:40:22 -0300

    update react-vanilla, jsx runtime

---
+++ examples/react-vanilla/server.test.js
@@ -0,0 +1,11 @@
+import test from 'node:test'
+import { makeBuildTest, makeIndexTest } from '../test-factories.mjs'
+import { main } from './server.js'
+
+const cwd = import.meta.dirname
+
+test('vue-vanilla', async (t) => {
+  await t.test('build production bundle', makeBuildTest({ cwd }))
+  await t.test('render index page in production', makeIndexTest({ main }))
+  await t.test('render index page in development', makeIndexTest({ main, dev: true }))
+})
