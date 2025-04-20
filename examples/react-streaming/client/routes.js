80fae935381e3cd41a1cc8624e78d096950888ea
Jonas Galvez <jonasgalvez@gmail.com>
2025-04-20 19:53:15 -0300

    reintroduce maintained react examples

---
+++ examples/react-streaming/client/routes.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default [
+  {
+    path: '/',
+    component: React.lazy(() => import('./views/index.jsx'))
+  },
+  {
+    path: '/other',
+    component: React.lazy(() => import('./views/other.jsx'))
+  }
+]
