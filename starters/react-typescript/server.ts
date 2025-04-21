0164927ba3fb9a68b87d0ca6f25ef76a54cc1d63
Jonas Galvez <jonasgalvez@gmail.com>
2025-04-20 21:06:10 -0300

    add react ts starter

---
+++ starters/react-typescript/server.ts
@@ -0,0 +1,39 @@
+import Fastify from 'fastify'
+import FastifyVite from '@fastify/vite'
+import FastifyFormBody from '@fastify/formbody'
+
+const server = Fastify({
+  logger: {
+    transport: {
+      target: '@fastify/one-line-logger'
+    }
+  }
+})
+
+await server.register(FastifyFormBody)
+await server.register(FastifyVite, {
+  root: import.meta.url,
+  renderer: '@fastify/react',
+})
+
+await server.vite.ready()
+
+server.decorate('db', {
+  todoList: [
+    'Do laundry',
+    'Respond to emails',
+    'Write report',
+  ]
+})
+
+server.put('/api/todo/items', (req, reply) => {
+  server.db.todoList.push(req.body)
+  reply.send({ ok: true })
+})
+
+server.delete('/api/todo/items', (req, reply) => {
+  server.db.todoList.splice(req.body, 1)
+  reply.send({ ok: true })
+})
+
+await server.listen({ port: 3000 })
