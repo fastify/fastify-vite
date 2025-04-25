e0a449bb4e2ec3a1ae7cac176731f09856c9c8f7
Andreas Johansson <z@zanmato.se>
2025-04-25 20:23:30 +0200

    feat(vue): add i18n starter
    This example overrides $app/index.js and provides new route creation as well as
    beforeEach handlers to enable locale prefix and domain routing.

---
+++ starters/vue-i18n/client/pages/wildcard/[slug+].vue
@@ -0,0 +1,39 @@
+<script>
+export const layout = 'default'
+
+export function getData ({ req }) {
+  let pathMatch = req.params['*'];
+  if (pathMatch.charAt(pathMatch.length - 1) == '/') {
+    pathMatch = pathMatch.substr(0, pathMatch.length - 1);
+  }
+
+  return {
+    pathMatch: pathMatch.split('/'),
+    locale: req.route.meta.locale,
+  }
+}
+
+// Define routes that aren't the default locale (en)
+export const i18n = {
+  'sv': '/asterisk/:slug+',
+  'da': '/asterisk/:slug+',
+}
+</script>
+
+<script setup>
+import { useData } from '$app/hooks'
+import { useI18nUtils } from '/composables/i18n'
+
+const { localePath } = useI18nUtils()
+
+const data = useData()
+</script>
+
+<template>
+  <h1>Wildcard example that matches /wildcard/*</h1>
+  <p>Path match: {{ data.pathMatch.join(', ') }}</p>
+  <p>Locale: {{ data.locale }}</p>
+  <p>
+    <router-link :to="localePath({ name: 'index' })">Go back to the index</router-link>
+  </p>
+</template>
