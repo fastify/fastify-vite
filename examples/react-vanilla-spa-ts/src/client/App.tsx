c312e57c939ff32f9bc60ecbacfbfca5f10c3761
Jonas Galvez <jonasgalvez@gmail.com>
2025-04-29 18:22:26 -0300

    @fastify/vite@8.1.1 (#248)
    * release: @fastify/vite@8.1.1
    
    * bump: @fastify/vite@8.1.1

---
+++ examples/react-vanilla-spa-ts/src/client/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1>Hello world from React and @fastify/vite!</h1>
+    </div>
+  )
+}
---
+++ examples/vue-vanilla-ts/server.ts
@@ -0,0 +1,50 @@
+
+import { fileURLToPath } from 'node:url'
+import { join } from 'node:path'
+import Fastify from 'fastify'
+import FastifyVite from '@fastify/vite'
+import { renderToString } from 'vue/server-renderer'
+import type { VNode } from 'vue'
+
+export async function main (dev?: boolean) {
+  const server = Fastify()
+
+  await server.register(FastifyVite, {
+    // Note: this is specific to using TypeScript for the
+    // Fastify server — which still requires a build step.
+    // Once Node.js can natively run TypeScript files without
+    // an experimental CLI flag, all this ceases to be necessary.
+    //
+    // Since the original server.ts sits at the top-level dir,
+    // and the compiled version in dist/server.js, we need
+    // to rely on process.cwd() to acurately determine the root dir
+    // upon boot — or we use a CLI path flag (you can still 
+    // use process.cwd() to resolve the absolute path from the flag)
+    root: process.cwd(),
+    distDir: join(process.cwd(), 'dist'),
+    dev: dev || process.argv.includes('--dev'),
+    async createRenderFunction ({ createApp }: { createApp: () => VNode}) {
+      return async () => ({
+        element: await renderToString(createApp())
+      })
+    }
+  })
+
+  server.setErrorHandler((err, req, reply) => {
+    console.error(err)
+    reply.send(err)
+  })
+
+  await server.vite.ready()
+
+  server.get('/', (req, reply) => {
+    return reply.html()
+  })
+
+  return server
+}
+
+if (process.argv[1] === fileURLToPath(import.meta.url)) {
+  const server = await main()
+  await server.listen({ port: 3000 })
+}
