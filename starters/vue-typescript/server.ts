a2893da50802d34ef154c5f3aae6f23ba2ce2879
Jonas Galvez <jonasgalvez@gmail.com>
2025-04-30 02:12:57 -0300

    release: @fastify/vue@1.1.0

--- starters/vue-typescript/server.ts
+++ starters/vue-typescript/server.ts
@@ -1,6 +1,11 @@
+
 import Fastify from 'fastify'
 import FastifyVite from '@fastify/vite'
 import FastifyFormBody from '@fastify/formbody'
+
+interface Database {
+  todoList: string[]
+}
 
 const server = Fastify({
   logger: {
@@ -10,7 +15,17 @@ const server = Fastify({
   }
 })
 
-server.decorate('db', {
+// @ts-ignore TODO
+await server.register(FastifyFormBody)
+
+await server.register(FastifyVite, {
+  root: import.meta.dirname,
+  renderer: '@fastify/vue',
+})
+
+await server.vite.ready()
+
+server.decorate<Database>('db', {
   todoList: [
     'Do laundry',
     'Respond to emails',
@@ -18,21 +33,20 @@ server.decorate('db', {
   ]
 })
 
-server.put('/api/todo/items', (req, reply) => {
-  server.db.todoList.push(req.body)
+server.put<{
+  Body: string
+}>('/api/todo/items', (req, reply) => {
+  const db = server.getDecorator<Database>('db')
+  db.todoList.push(req.body)
   reply.send({ ok: true })
 })
 
-server.delete('/api/todo/items', (req, reply) => {
-  server.db.todoList.splice(req.body, 1)
+server.delete<{
+  Body: number
+}>('/api/todo/items', (req, reply) => {
+  const db = server.getDecorator<Database>('db')
+  db.todoList.splice(req.body, 1)
   reply.send({ ok: true })
 })
 
-await server.register(FastifyFormBody)
-await server.register(FastifyVite, {
-  root: import.meta.url,
-  renderer: '@fastify/vue',
-})
-
-await server.vite.ready()
 await server.listen({ port: 3000 })
