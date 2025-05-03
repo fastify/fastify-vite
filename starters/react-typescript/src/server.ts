11d745bfcc205555e3a03bc512de6e3f98aa5121
Wei Wang <wei@fastmail.com>
2025-05-03 14:33:07 -0400

    Add src directory to starters/react-typescript

--- starters/react-typescript/src/server.ts
+++ starters/react-typescript/src/server.ts
@@ -1,7 +1,7 @@
+import { resolve } from 'node:path'
 import Fastify from 'fastify'
 import FastifyVite from '@fastify/vite'
 import FastifyFormBody from '@fastify/formbody'
-
 interface Database {
   todoList: string[]
 }
@@ -18,7 +18,8 @@ await server.register(FastifyFormBody)
 
 await server.register(FastifyVite, {
   // TODO handle via CLI path argument with proper resolve
-  root: process.cwd(),
+  root: resolve(import.meta.dirname, '..'),
+  distDir: import.meta.dirname, // This file will also live in the dist folder when built
   renderer: '@fastify/react',
 })
 
--- starters/vue-typescript/client/layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="layout">
-    <slot />
-  </div>
-</template>
