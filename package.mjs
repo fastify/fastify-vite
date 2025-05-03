1e964091bd61df0dc5442be067083a73c00bbbf2
Wei Wang <wei@fastmail.com>
2025-05-02 20:24:41 -0400

    fix: Remove optionalDependencies from @fastify/vite (#257)

--- package.mjs
+++ package.mjs
@@ -6,7 +6,83 @@ import { readFileSync, writeFileSync } from 'node:fs'
 
 const root = path.resolve(__dirname)
 
+const fastifyViteVersion = getVersion('fastify-vite')
+const fastifyVueVersion = getVersion('fastify-vue')
+const fastifyReactVersion = getVersion('fastify-react')
+
+const starters = [
+  'react-base',
+  'react-kitchensink',
+  'react-typescript',
+  'vue-base',
+  'vue-kitchensink',
+  'vue-typescript',
+]
+
 if (process.argv.includes('--test')) {
+  await runAllTests()
+}
+
+if (process.argv.includes('--prep-for-dev')) {
+  await prepForDev()
+}
+
+if (process.argv.includes('--prep-for-release')) {
+  await prepForRelease()
+}
+
+async function prepForRelease () {
+  const starterRoot = join(root, 'starters')
+  cd(starterRoot)
+  // Remove optionalDependencies from @fastify/vite's package.json
+  let mainPkgJSONPath = join(root, 'packages', 'fastify-vite', 'package.json')
+  let pkgJSON = JSON.parse(readFileSync(mainPkgJSONPath))
+  delete pkgJSON.optionalDependencies
+  writeFileSync(mainPkgJSONPath, JSON.stringify(pkgJSON, null, 2))
+  // Replace workspace:^ with hard versions referenced in starter package.json files
+  for (const starter of starters) {
+    pkgJSON = JSON.parse(readFileSync(join(starterRoot, starter, 'package.json')))
+    pkgJSON.dependencies['@fastify/vite'] = `^${fastifyViteVersion}`
+    if (pkgJSON.dependencies['@fastify/vue']) {
+      pkgJSON.dependencies['@fastify/vue'] = `^${fastifyVueVersion}`
+    }
+    if (pkgJSON.dependencies['@fastify/react']) {
+      pkgJSON.dependencies['@fastify/react'] = `^${fastifyReactVersion}`
+    }
+    writeFileSync(join(starterRoot, starter, 'package.json'), JSON.stringify(pkgJSON, null, 2))
+  }
+  process.exit()
+}
+
+async function prepForDev () {
+  const starterRoot = join(root, 'starters')
+  cd(starterRoot)
+  // Add optionalDependencies to @fastify/vite's package.json
+  let mainPkgJSONPath = join(root, 'packages', 'fastify-vite', 'package.json')
+  let pkgJSON = JSON.parse(readFileSync(mainPkgJSONPath))
+  pkgJSON.optionalDependencies = {
+    '@fastify/vue': 'workspace:^',
+    '@fastify/react': 'workspace:^',
+    'vite': 'latest'
+  }
+  writeFileSync(mainPkgJSONPath, JSON.stringify(pkgJSON, null, 2))
+  for (const starter of starters) {
+    pkgJSON = JSON.parse(readFileSync(join(starterRoot, starter, 'package.json')))
+    pkgJSON.dependencies['@fastify/vite'] = 'workspace:^'
+    if (pkgJSON.dependencies['@fastify/vue']) {
+      pkgJSON.dependencies['@fastify/vue'] = 'workspace:^'
+    }
+    if (pkgJSON.dependencies['@fastify/react']) {
+      pkgJSON.dependencies['@fastify/react'] = 'workspace:^'
+    }
+    writeFileSync(join(starterRoot, starter, 'package.json'), JSON.stringify(pkgJSON, null, 2))
+  }
+  cd(root)
+  await $`pnpm i`
+  process.exit()
+}
+
+async function runAllTests () {
   cd(join(root, 'packages/fastify-vite'))
 
   await $`npx vitest run`
@@ -30,68 +106,6 @@ if (process.argv.includes('--test')) {
     ]) {
       cd(join(root, 'examples', example))
       await $`node --test`
-    }
-  }
-  process.exit()
-}
-
-const fastifyViteVersion = getVersion('fastify-vite')
-const fastifyVueVersion = getVersion('fastify-vue')
-const fastifyReactVersion = getVersion('fastify-react')
-
-if (process.argv.includes('--prep-for-dev')) {
-  const starterRoot = join(root, 'starters')
-
-  cd(starterRoot)
-
-  if (process.stdout.isTTY) {
-    for (const starter of [
-      'react-base',
-      'react-kitchensink',
-      // 'react-typescript',
-      'vue-base',
-      'vue-kitchensink',
-      // 'vue-typescript',
-    ]) {
-      const pkgJSON = JSON.parse(readFileSync(join(starterRoot, starter, 'package.json')))
-      pkgJSON.dependencies['@fastify/vite'] = 'workspace:^'
-      if (pkgJSON.dependencies['@fastify/vue']) {
-        pkgJSON.dependencies['@fastify/vue'] = 'workspace:^'
-      }
-      if (pkgJSON.dependencies['@fastify/react']) {
-        pkgJSON.dependencies['@fastify/react'] = 'workspace:^'
-      }
-      writeFileSync(join(starterRoot, starter, 'package.json'), JSON.stringify(pkgJSON, null, 2))
-    }
-    cd(root)
-    await $`pnpm i`
-  }
-  process.exit()
-}
-
-if (process.argv.includes('--prep-for-release')) {
-  const starterRoot = join(root, 'starters')
-
-  cd(starterRoot)
-
-  if (process.stdout.isTTY) {
-    for (const starter of [
-      'react-base',
-      'react-kitchensink',
-      // 'react-typescript',
-      'vue-base',
-      'vue-kitchensink',
-      // 'vue-typescript',
-    ]) {
-      const pkgJSON = JSON.parse(readFileSync(join(starterRoot, starter, 'package.json')))
-      pkgJSON.dependencies['@fastify/vite'] = fastifyViteVersion
-      if (pkgJSON.dependencies['@fastify/vue']) {
-        pkgJSON.dependencies['@fastify/vue'] = fastifyVueVersion
-      }
-      if (pkgJSON.dependencies['@fastify/react']) {
-        pkgJSON.dependencies['@fastify/react'] = fastifyReactVersion
-      }
-      writeFileSync(join(starterRoot, starter, 'package.json'), JSON.stringify(pkgJSON, null, 2))
     }
   }
   process.exit()
