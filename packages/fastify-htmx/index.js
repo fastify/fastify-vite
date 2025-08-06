0e98c5636630ddd64249b765d273314554b06a36
Jan Monschke <janmonschke@fastmail.com>
2025-08-06 22:34:13 +0200

    chore(fastify-htmx): remove unused variables/parameters (#281)

--- packages/fastify-htmx/index.js
+++ packages/fastify-htmx/index.js
@@ -107,7 +107,7 @@ async function prepareClient(clientModule, scope, config) {
 }
 
 // The return value of this function gets registered as reply.html()
-export function createHtmlFunction(source, scope, config) {
+export function createHtmlFunction(source, _, config) {
   const htmlTemplate = config.createHtmlTemplateFunction(source)
   return function (ctx) {
     this.type('text/html')
@@ -116,7 +116,7 @@ export function createHtmlFunction(source, scope, config) {
   }
 }
 
-export function createRouteHandler({ client, route }, scope, config) {
+export function createRouteHandler({ client, route }, scope) {
   if (route.fragment) {
     return async (req, reply) => {
       req.route = route
--- packages/fastify-htmx/virtual/layouts/default.jsx
+++ packages/fastify-htmx/virtual/layouts/default.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from '@kitajs/html/suspense.js'
 // This file serves as a placeholder if no layout.jsx file is provided
 
-export default function Layout({ app, req, reply, children }) {
+export default function Layout({ children }) {
   return <Suspense>{children}</Suspense>
 }
