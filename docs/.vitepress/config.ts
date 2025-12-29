2975db75bfaa7716af69268b8f825548cf0da7f8
Wei Wang <wei@fastmail.com>
2025-12-29 12:28:15 -0500

    Remove unused dependencies in docs folder

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 import { withMermaid } from 'vitepress-plugin-mermaid'
 
-export default withMermaid({
+const config = withMermaid({
   lang: 'en-US',
   title: '@fastify/vite',
   description: 'Vite & Vue powered static site generator.',
@@ -64,6 +64,24 @@ export default withMermaid({
     },
   }
 })
+
+// https://github.com/emersonbottero/vitepress-plugin-mermaid/issues/83
+// Filter out transitive dependencies that pnpm doesn't expose. Otherwise we would have to install
+// them into package.json just to make vitepress-plugin-mermaid work without warnings.
+if (config.vite?.optimizeDeps?.include) {
+  const EXCLUDED_DEPS = new Set([
+    '@braintree/sanitize-url',
+    'dayjs',
+    'debug',
+    'cytoscape-cose-bilkent',
+    'cytoscape'
+  ])
+  config.vite.optimizeDeps.include = config.vite.optimizeDeps.include.filter(
+    (dep) => !EXCLUDED_DEPS.has(dep)
+  )
+}
+
+export default config
 
 function nav(): DefaultTheme.NavItem[] {
   return [
