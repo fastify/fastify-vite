7dcce09c3aff44a514b420dc1d74ed3d06fb2afd
Wei Wang <wei@fastmail.com>
2026-01-10 16:37:44 -0500

    Delete duplicate server.test.ts file in vue-vanilla-ts example

--- examples/vue-vanilla-ts/src/server.test.ts
+++ examples/vue-vanilla-ts/src/server.test.ts
@@ -3,10 +3,11 @@ import { makeBuildTest, makeIndexTest } from '../../test-factories.mjs'
 import { main } from './server.ts'
 import { resolve } from 'node:path'
 
-const cwd = resolve(import.meta.dirname, '..')
-
 test('vue-vanilla', async (t) => {
-  await t.test('build production bundle', makeBuildTest({ cwd }))
+  await t.test(
+    'build production bundle',
+    makeBuildTest({ cwd: resolve(import.meta.dirname, '..') }),
+  )
   await t.test('render index page in development', makeIndexTest({ main, dev: true }))
   await t.test('render index page in production', makeIndexTest({ main, dev: false }))
 })
