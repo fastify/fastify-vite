342b49a04ccdee5fc70f84d8eae1d7c6dc0b9cb5
Wei Wang <wei@fastmail.com>
2026-01-19 10:23:55 -0500

    Convert utils.js -> utils.ts

--- packages/fastify-vite/src/utils.js
+++
@@ -1,6 +0,0 @@
-const { ensureConfigFile, ejectBlueprint } = require('./setup.ts')
-const { createHtmlTemplateFunction } = require('./html.ts')
-
-module.exports.createHtmlTemplateFunction = createHtmlTemplateFunction
-module.exports.ensureConfigFile = ensureConfigFile
-module.exports.ejectBlueprint = ejectBlueprint
